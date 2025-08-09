abfb791cc09f2d4383de4fffa7e6a9ec29237bb1
muslmasan <muslmasan3@gmail.com>
2025-08-09 19:22:18 +0200

    finishing the singup ui next is adding the functionality

--- lib/features/auth/presentation/signup/sign_up.dart
+++ lib/features/auth/presentation/signup/sign_up.dart
@@ -73,11 +73,12 @@ class _SignUpState extends State<SignUp> {
                               fontSize: 13
                           ),
                         ),
-                        SizedBox(height: 56,),
+                        SizedBox(height: 32,),
                         Text(
                           'Sign Up For Free',
                           style: TextStyle(
-                              fontFamily: 'bentonBold'
+                              fontFamily: 'bentonBold',
+                            fontSize: 24
                           ),
                         ),
                         SizedBox(height: 32,),
@@ -93,23 +94,36 @@ class _SignUpState extends State<SignUp> {
                               ),
                             ],
                           ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              labelStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide.none
-                              ),
-                              fillColor: Colors.white,
-
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/images/person.svg',
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(width:8,),
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Escanoo..|',
+                                      labelStyle: TextStyle(color: Colors.grey),
+                                      border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(15),
+                                          borderSide: BorderSide.none
+                                      ),
+                                      fillColor: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
 
                         SizedBox(height: 16,),
                         Container(
-                          height: 57,
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(25.0),
@@ -121,20 +135,126 @@ class _SignUpState extends State<SignUp> {
                               ),
                             ],
                           ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              labelStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide.none
-                              ),
-                              fillColor: Colors.white,
-
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/images/mail.svg',
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(width:8,),
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Email',
+                                      labelStyle: TextStyle(color: Colors.grey),
+                                      border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(15),
+                                          borderSide: BorderSide.none
+                                      ),
+                                      fillColor: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-
+                        SizedBox(height: 16,),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(25.0),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(5, 10),
+                                blurRadius: 10.0,
+                                color: Color(0xFF5A6CEA).withAlpha((0.1*255).toInt()),
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/images/lock.svg',
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(width:8,),
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Password',
+                                      labelStyle: TextStyle(color: Colors.grey),
+                                      border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(15),
+                                          borderSide: BorderSide.none
+                                      ),
+                                      fillColor: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                SvgPicture.asset(
+                                  'assets/images/eye.svg',
+                                  width: 30,
+                                  height: 30,
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(width:8,),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16,),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0,),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/images/check.svg',
+                                width: 30,
+                                height: 30,
+                                fit: BoxFit.fill,
+                              ),
+                              SizedBox(width:8,),
+                              Text(
+                                'Keep Me Signed in',
+                                style: TextStyle(
+                                  fontFamily: 'bentonRegular',
+                                  fontSize: 16,
+                                  color: Colors.grey
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 16,),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/images/check.svg',
+                                width: 30,
+                                height: 30,
+                                fit: BoxFit.fill,
+                              ),
+                              SizedBox(width:8,),
+                              Text(
+                                'Email Me About Special Pricing',
+                                style: TextStyle(
+                                    fontFamily: 'bentonRegular',
+                                    fontSize: 16,
+                                    color: Colors.grey
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                         SizedBox(height: 24,),
                         Container(
                           decoration: BoxDecoration(
@@ -185,3 +305,6 @@ class _SignUpState extends State<SignUp> {
     );
   }
 }
+
+
+//TODO: adding the functionality to the singup
